10c1e4a1f167d6521099088ce2b18b7e859de567
lin chi kin <b11002078@g.chu.edu.tw>
2023-04-28 12:08:50 +0800

    modified:   20230410-homework.sql

--- 20230410-homework.sql
+++ 20230410-homework.sql
@@ -170,35 +170,21 @@ select CustomerID,ContactName,ProductID
 from t1
 where (RowNum=any(select m from t2));	
 -- 找出有敗過銷售金額前三高個產品的前三個大客戶
-with t1 as (
-	select 
-		UnitPrice*(1-Discount)*Quantity as OrderPrice,
-		ProductID
-	from [Order Details]
-),
-t2 as (
-	select top 3 ProductID,sum(OrderPrice) as Revenue
-	from t1
-	group by ProductID
-	order by Revenue desc
-),
-t3 as (
-	select
-		row_number() over (order by od.ProductID ,od.Quantity) as RowNum,
-		c.CustomerID,c.ContactName,od.ProductID,od.Quantity
-	from Customers c
-	inner join Orders o on (o.CustomerID=c.CustomerID)
-	inner join [Order Details] od on (od.OrderID=o.OrderID)
-	where (od.ProductID=any(select ProductID from t2))
-),
-t4 as (
-	select max(RowNum) as Top3P
-	from t3
-	group by ProductID
+select distinct top 3
+	c.CustomerID,c.CompanyName,
+	sum(od.UnitPrice*od.Quantity*(1-od.Discount))as SalesAmount
+from Customers c
+inner join Orders o on (o.CustomerID=c.CustomerID)
+inner join [Order Details] od on (od.OrderID=o.OrderID)
+where od.ProductID in (
+	select top 3
+		od.ProductID
+	from [Order Details] od
+	group by od.ProductID
+	order by sum((od.UnitPrice*od.Quantity)*(1-od.Discount)) desc
 )
-select CustomerID,ContactName,ProductID,Quantity  
-from t3
-where (RowNum=any(select Top3P from t4));
+group by c.CustomerID,c.CompanyName
+order by SalesAmount desc;
 -- 找出有敗過銷售金額前三高個產品所屬類別的前三個大客戶
 with t1 as (
 	select 
@@ -315,7 +301,7 @@ with t1 as (
 	from Products p
 	inner join [Order Details] od on (od.ProductID=p.ProductID)
 )
-select *
+select p.ProductID,p.ProductName
 from Products p
 where (not exists(select * from t1 where (t1.ProductID=p.ProductID)));
 -- 列出沒有傳真 (Fax) 的客戶和它的消費總金額
@@ -330,7 +316,10 @@ select CustomerID,sum(ConsumPrice)
 from t1
 group by CustomerID;
 -- 列出每一個城市消費的產品種類數量
-
+select p.ProductID,sum(od.Quantity) as SalesAmount
+from [Order Details] od
+inner join Products p on (p.ProductID=od.ProductID)
+group by p.ProductID;
 -- 列出目前沒有庫存的產品在過去總共被訂購的數量
 
 -- 列出目前沒有庫存的產品在過去曾經被那些客戶訂購過
@@ -338,14 +327,94 @@ group by CustomerID;
 -- 列出每位員工的下屬的業績總金額
 
 -- 列出每家貨運公司運送最多的那一種產品類別與總數量
-
+with SumOfEachProBySup as (
+	select 
+		s.SupplierID,
+		p.ProductID,
+		sum(od.Quantity) as Qty 
+	from Suppliers s
+	inner join Products p on (p.SupplierID=s.SupplierID)
+	inner join [Order Details] od on (od.ProductID=p.ProductID)
+	group by s.SupplierID,p.ProductID
+),
+MaxOfEachProBySup as (
+	select SupplierID,max(Qty) as MaxQty
+	from SumOfEachProBySup 
+	group by SupplierID
+)
+select *
+from SumOfEachProBySup ss
+where (ss.Qty=any(select MaxQty from MaxOfEachProBySup where SupplierID=ss.SupplierID));
 -- 列出每一個客戶買最多的產品類別與金額
-
+with MostQrtByCust as (
+	select 
+		c.CustomerID,
+		max(od.Quantity) as QtyPerCust
+	from Customers c
+	inner join Orders o on (o.CustomerID=c.CustomerID)
+	inner join [Order Details] od on (od.OrderID=o.OrderID)
+	group by c.CustomerID
+),
+MostPurchaseByCust as (
+	select o.CustomerID,p.CategoryID,od.Quantity*od.UnitPrice*(1-od.Discount)as Price
+	from Orders o
+	inner join [Order Details] od on (od.OrderID=o.OrderID)
+	inner join Products p on (p.ProductID=od.ProductID)
+	where (
+		o.CustomerID=any(select CustomerID from MostQrtByCust where CustomerID=o.CustomerID)
+		and
+		od.Quantity=any(select QtyPerCust from MostQrtByCust where CustomerID=o.CustomerID)
+	)
+)
+select * from MostPurchaseByCust;
 -- 列出每一個客戶買最多的那一個產品與購買數量
-
+with MostQrtByCust as (
+	select 
+		c.CustomerID,
+		max(od.Quantity) as QtyPerCust
+	from Customers c
+	inner join Orders o on (o.CustomerID=c.CustomerID)
+	inner join [Order Details] od on (od.OrderID=o.OrderID)
+	group by c.CustomerID
+),
+MostPurchaseByCust as (
+	select o.CustomerID,od.ProductID,p.ProductName,od.Quantity
+	from Orders o
+	inner join [Order Details] od on (od.OrderID=o.OrderID)
+	inner join Products p on (p.ProductID=od.ProductID)
+	where (
+		o.CustomerID=any(select CustomerID from MostQrtByCust where CustomerID=o.CustomerID)
+		and
+		od.Quantity=any(select QtyPerCust from MostQrtByCust where CustomerID=o.CustomerID)
+	)
+)
+select * from MostPurchaseByCust;
 -- 按照城市分類，找出每一個城市最近一筆訂單的送貨時間
-
+select c.City,max(o.ShippedDate) 
+from Customers c
+inner join Orders o on (o.CustomerID=c.CustomerID)
+group by c.City;
 -- 列出購買金額第五名與第十名的客戶，以及兩個客戶的金額差距
-
-
-
+with PriceOD as (
+	select *,UnitPrice*Quantity*(1-Discount) as Price
+	from [Order Details]
+),
+AggPricePerCust as (
+	select o.CustomerID,sum(Price) as AggPrice
+	from PriceOD od
+	inner join Orders o on (o.OrderID=od.OrderID)
+	group by o.CustomerID
+),
+Selection as (
+	select row_number() over (order by AggPrice desc) as RowNum , * 
+	from AggPricePerCust
+),
+Selection2 as (
+	select max(AggPrice)-min(AggPrice) as Differences 
+	from Selection
+	where (RowNum in (5,10))
+)
+select *
+from Selection ss
+full join Selection2 sss on (sss.Differences>0)
+where (ss.RowNum in (5,10))
